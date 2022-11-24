0a6bf166791acade7f9d536f1a8b8201405ac092
Silavsale <silavsale@gmail.com>
2022-11-24 13:06:09 -0500

    add docker, trigger task on save to rebuild docker container on save

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
-const port = Number(process.env.PORT || 8080);
+const port = Number(process.env.PORT || 8000);
+console.log('process.env.PORT', process.env.PORT);
 
 const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({ hello: 'world' });
+  res.json({ hello: 'Peremoga!!!' });
 });
 
 app.listen(port, '0.0.0.0', () => {
